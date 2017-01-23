b47f769304ea07d031b7630e9a330a0a903ba67d
huijinh <huijinh@pptv.com>
2017-01-23 14:50:57 +0800

    first

--- app/component/CommonNav.js
+++ app/component/CommonNav.js
@@ -4,5 +4,40 @@
 import React,{ Component, PropTypes } from 'react'
 import {
     Text,
-    React
+    View,
+    TouchableOpacity,
+    Dimensions
+} from 'react-native'
+const {height, width} = Dimensions.get('window')
+export default class CommonNav extends Component{
+    static propTypes ={
+        title:PropTypes.string,
+        doSomething:null,
+        canBack:file
+    }
+    static defaultProps ={
+        title:'',
+        doSomething:null,
+        canBack:false
+    }
+
+    render(){
+        return(
+            <View style={}>
+                {
+                    this.props.canBack
+                    ?
+                    <View sytle={}>
+                        <TouchableOpacity activeOpacity={0.5} onPress={this.props.doSomething}>
+                            <Text style={styles.backTitleStyle}>返回</Text>
+                        </TouchableOpacity>
+                    </View>
+                    :
+                    null
+                }
+                <Text style = {}
+            </View>
+        )
+    }
+
 }
